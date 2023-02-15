e29e640e6ebb493e61bdeb64927e931ed0440b30
nozari <dev.nozari@gmail.com>
2023-02-15 14:05:48 +0330

    Initial React RealEstate Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import './App.css';
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Offers from "./pages/Offers";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/profile" element={<Profile />}></Route>
+              <Route path="/signIn" element={<SignIn />}></Route>
+              <Route path="/signUp" element={<SignUp />}></Route>
+              <Route path="/offers" element={<Offers />}></Route>
+              <Route path="/forgotPassword" element={<ForgotPassword />}></Route>
+          </Routes>
+      </Router>
   );
 }
-
 export default App;
