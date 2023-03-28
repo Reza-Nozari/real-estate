8490bdf95f34380d7ecdbf2591db088525d926eb
nozari <dev.nozari@gmail.com>
2023-03-28 17:43:27 +0430

    Implement Profile && PrivateRoute && Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
@@ -7,20 +7,23 @@ import SignUp from "./pages/SignUp";
 import Offers from "./pages/Offers";
 import ForgotPassword from "./pages/ForgotPassword";
 import Header from "./components/header/Header";
+import PrivateRoute from "./components/route/PrivateRoute";
 
 function App() {
   return (
-      <Router>
-          <Header/>
-          <Routes>
-              <Route path="/" element={<Home />}></Route>
-              <Route path="/profile" element={<Profile />}></Route>
-              <Route path="/signIn" element={<SignIn />}></Route>
-              <Route path="/signUp" element={<SignUp />}></Route>
-              <Route path="/offers" element={<Offers />}></Route>
-              <Route path="/forgotPassword" element={<ForgotPassword />}></Route>
-          </Routes>
-      </Router>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/profile" element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />}></Route>
+        </Route>
+        <Route path="/signIn" element={<SignIn />}></Route>
+        <Route path="/signUp" element={<SignUp />}></Route>
+        <Route path="/offers" element={<Offers />}></Route>
+        <Route path="/forgotPassword" element={<ForgotPassword />}></Route>
+      </Routes>
+    </Router>
   );
 }
 export default App;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,83 @@
-export default  function Profile() {
-    return (<div>
-        Profile
-    </div>);
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import "react-toastify/dist/ReactToastify.css";
+import { getAuth } from "firebase/auth";
+
+export default function Profile() {
+  const auth = getAuth();
+  const navigate = useNavigate();
+
+  if (!auth || !auth.currentUser) {
+    navigate("/signIn");
+  }
+
+  const [formData, setFormData] = useState({
+    email: auth.currentUser.email,
+    name: auth.currentUser.displayName,
+  });
+
+  const { email, name } = formData;
+
+  function onChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  function onLogOut() {
+    auth.signOut();
+    navigate("/signIn");
+  }
+
+  return (
+    <div className="bg-[#f0f8ed] h-full">
+      <div className="max-w-[500px] mx-auto">
+        <div className="flex flex-col  gap-6 px-2 pb-5">
+          <div className="flex justify-center align-middle mt-3">
+            <h1 className="text-4xl font-bold">My Profile</h1>
+          </div>
+          <div className="flex flex-col justify-center align-middle gap-3">
+            <div className="flex justify-center align-middle">
+              <input
+                type="text"
+                placeholder="Please Insert Name"
+                className="rounded-lg w-full bg-white"
+                value={name}
+                name="name"
+                onChange={onChange}
+              />
+            </div>
+
+            <div className="flex justify-center align-middle">
+              <input
+                type="text"
+                placeholder="Please Insert Name"
+                className="rounded-lg w-full bg-white"
+                value={email}
+                name="email"
+                onChange={onChange}
+              />
+            </div>
+            <div className="flex">
+              <div className="flex flex-1 gap-1">
+                <p>Do want to change your name?</p>
+                <span className="text-orange-400 hover:text-orange-600 font-bold">
+                  Edit
+                </span>
+              </div>
+              <div className="flex  flex-row-reverse gap-1">
+                <span
+                  onClick={onLogOut}
+                  className="text-blue-400 hover:text-blue-600 font-bold cursor-pointer"
+                >
+                  Sign out
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
